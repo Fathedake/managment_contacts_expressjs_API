f6d11ddb256857e44452b9a273911c294b0663c7
Fathedake <facie678@gmail.com>
2023-11-01 13:24:51 +0100

    update projet 13:25

--- index.js
+++ index.js
@@ -59,8 +59,9 @@ app.use(
     keys: ["COOKIE_SECRET"], 
    // maxAge: 2592000000,
     secure: process.env.NODE_ENV === 'development' ? false : true,
-    httpOnly:false /*process.env.NODE_ENV === 'development' ? false : true*/,
+    httpOnly:process.env.NODE_ENV === 'development' ? false : true,
     sameSite: process.env.NODE_ENV === 'development' ? false : 'none',
+    domain: ".vercel.app",
   }),
 );
 
